ee4a2beb8a822f1131e382bbd5fdb32d8385d1df
bouboss <marytra22@gmail.com>
2024-09-16 05:51:40 +0000

    modif

--- src/app/views/pages/connexion/connexion.component.ts
+++ src/app/views/pages/connexion/connexion.component.ts
@@ -1,61 +1,37 @@
-import { CommonModule } from '@angular/common'; // Import CommonModule for ngStyle and other common directives
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router'; // Import RouterModule for navigation
-import {
-  ButtonDirective,
-  CardBodyComponent,
-  CardComponent,
-  CardGroupComponent,
-  ColComponent,
-  ContainerComponent,
-  FormControlDirective,
-  FormDirective,
-  InputGroupComponent,
-  InputGroupTextDirective,
-  RowComponent,
-  TextColorDirective,
-} from '@coreui/angular';
-import { IconDirective } from '@coreui/icons-angular';
-import { Login } from '../../../models/login';
+import { Router } from '@angular/router';
+import { ButtonModule, CardModule, FormModule, GridModule } from '@coreui/angular';
+import { IconModule } from '@coreui/icons-angular';
+import Swal from 'sweetalert2';
 import { AuthService } from '../../../services/auth.services';
 import { StorageService } from '../../../services/storage.services';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: 'app-connexion',
   standalone: true,
-  imports: [
-    CommonModule, // Ensure CommonModule is imported here
+  imports: [ CommonModule,
+    CardModule,
+    ButtonModule,
+    GridModule,
+    IconModule,
+    FormModule,
     FormsModule,
-    RouterModule, // Ensure RouterModule is imported here
-    ContainerComponent,
-    RowComponent,
-    ColComponent,
-    CardGroupComponent,
-    CardComponent,
-    CardBodyComponent,
-    FormDirective,
-    InputGroupComponent,
-    InputGroupTextDirective,
-    IconDirective,
-    FormControlDirective,
-    ButtonDirective,
-    TextColorDirective,
-  ],
+    HttpClientModule],
+  templateUrl: './connexion.component.html',
+  styleUrl: './connexion.component.scss'
 })
-export class LoginComponent implements OnInit {
-  type= true;
-  changeType(){
-    this.type = !this.type;
-  }
-  signinData: Login = {usernameOrEmail: '', password: ''};
+
+
+export class ConnexionComponent implements OnInit  {
+
+  // signinData: Login = {usernameOrEmail: '', password: ''};
   popupResult!: boolean;
   popupMessage = '';
 
   showAdminBoard = false;
-  showModeratorBoard = false;
   showUserBoard = false;
   username?: string;
 
@@ -75,28 +51,34 @@ export class LoginComponent implements OnInit {
     private storage: StorageService
   ) {}
 
+
+
   ngOnInit(): void {
     this.isLoggedIn = !!this.storage.getToken();
+
     if (this.isLoggedIn) {
       const user = this.storage.getUser();
       this.roles = user.roles;
+
       this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
       this.showUserBoard = this.roles.includes('ROLE_USER');
       this.username = user.username;
     }
+
     if (this.storage.getToken()) {
       this.isLoggedIn = true;
       this.roles = this.storage.getUser().roles;
     }
   }
 
-
   onSubmit(): void {
     const { username, password } = this.form;
+
     this.authService.login(username, password).subscribe({
       next: (data) => {
         this.storage.saveToken(data.accessToken);
         this.storage.saveUser(data);
+
         this.isLoginFailed = false;
         this.isLoggedIn = true;
         this.roles = this.storage.getUser().roles;
@@ -106,6 +88,7 @@ export class LoginComponent implements OnInit {
       error: (err) => {
         this.errorMessage = err.error.message;
         this.isLoginFailed = true;
+        this.showErrorPopup(this.errorMessage);
       },
     });
   }
@@ -116,11 +99,20 @@ export class LoginComponent implements OnInit {
   }
 
   navigate() {
-    this.router.navigateByUrl('/dashboard/accueil');
+    this.router.navigateByUrl('/dashboard');
   }
 
   reloadPage(): void {
     window.location.reload();
   }
-  
+
+  showErrorPopup(message: string): void {
+    Swal.fire({
+      icon: 'error',
+      title: 'Erreur',
+      text: message,
+      confirmButtonText: 'Réessayer'
+    });
+  }
+
 }
